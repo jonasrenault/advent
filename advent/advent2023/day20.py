3e899ed3711ee67b5728fc8c97d5f6c3e7b0baae
Jonas Renault <jonasrenault@gmail.com>
2024-02-29 10:54:00 +0100

    2023 day20-21

---
+++ advent/advent2023/day20.py
@@ -0,0 +1,184 @@
+from abc import ABC, abstractmethod
+from collections import defaultdict, deque
+from collections.abc import Iterable
+from math import lcm
+
+from tqdm import tqdm
+
+from advent.utils.utils import Advent
+
+advent = Advent(20, 2023)
+
+
+class Module(ABC):
+    @abstractmethod
+    def process(self, pulse: str, sender: str) -> None | list[tuple[str, str]]:
+        pass
+
+
+class FlipFlop(Module):
+    def __init__(self, children: Iterable[str]) -> None:
+        self.children = children
+        self.on = False
+
+    def process(self, pulse: str, sender: str) -> None | list[tuple[str, str]]:
+        if pulse == "high":
+            return None
+        self.on = not self.on
+        return [(child, "high" if self.on else "low") for child in self.children]
+
+    def __str__(self) -> str:
+        return "%" + ("on" if self.on else "off") + " -> " + ",".join(self.children)
+
+    def __repr__(self) -> str:
+        return "%" + ("on" if self.on else "off") + " -> " + ",".join(self.children)
+
+
+class Broadcast(Module):
+    def __init__(self, children: Iterable[str]) -> None:
+        self.children = children
+
+    def process(self, pulse: str, sender: str) -> list[tuple[str, str]]:
+        return [(child, pulse) for child in self.children]
+
+
+class Conjunction(Module):
+    def __init__(self, children: Iterable[str]) -> None:
+        self.children = children
+        self.memory: dict[str, str] = {}
+
+    def set_inputs(self, inputs: Iterable[str]):
+        for input in inputs:
+            self.memory[input] = "low"
+
+    def process(self, pulse: str, sender: str) -> list[tuple[str, str]]:
+        self.memory[sender] = pulse
+        if "low" in self.memory.values():
+            send = "high"
+        else:
+            send = "low"
+        return [(child, send) for child in self.children]
+
+    def __str__(self) -> str:
+        return (
+            "&" + " > " + ", ".join(self.memory.keys()) + "&->" + ", ".join(self.children)
+        )
+
+    def __repr__(self) -> str:
+        return (
+            "&" + " > " + ", ".join(self.memory.keys()) + "&->" + ", ".join(self.children)
+        )
+
+
+def main():
+    lines = advent.get_input_lines()
+    modules = get_modules(lines)
+    low = 0
+    high = 0
+    for _ in tqdm(range(1000)):
+        l, h, _ = press_btn(modules)  # noqa: E741
+        low += l
+        high += h
+    advent.submit(1, low * high)
+
+    # qb sends to rx, will send a low pulse when all its inputs have sent a high pulse.
+    # find loops for qb's inputs sending high pulses, then get LCM
+    loops = []
+    # for input in modules["qb"].memory.keys():
+    for input in modules["dn"].memory.keys():
+        pulse = "high"
+        c = find_loops(lines, pulse, input)
+        loops.append(c)
+    advent.submit(2, lcm(*loops))
+
+
+def find_loops(
+    lines: list[str], stop_pulse: str | None = None, stop_sender: str | None = None
+) -> int:
+    """
+    Find number of button pushes required for stop_sender to send stop_pulse.
+
+    Args:
+        lines (list[str]): the problem input
+        stop_pulse (str | None, optional): the pulse. Defaults to None.
+        stop_sender (str | None, optional): the sender. Defaults to None.
+
+    Returns:
+        int: number of button pushes required
+    """
+    modules = get_modules(lines)
+    c = 0
+    sent = False
+    while not sent:
+        c += 1
+        _, _, sent = press_btn(modules, stop_pulse, stop_sender)
+    return c
+
+
+def press_btn(
+    modules: dict[str, Module],
+    stop_pulse: str | None = None,
+    stop_sender: str | None = None,
+) -> tuple[int, int, bool]:
+    """
+    Press button module, sending a low pulse to broadcaster module and all processing all
+    subsequent pulses. Returns the count of low and high pulses sent. Function will return
+    True as third tuple element if stop_pulse is provided and was sent as a result of
+    pushing the button.
+
+    Args:
+        modules (dict[str, Module]): the modules
+        stop_pulse (str | None, optional): stop pulse to look for. Defaults to None.
+        stop_sender (str | None, optional): sender. Defaults to None.
+
+    Returns:
+        tuple[int, int, bool]: high and low counts
+    """
+    queue: deque[tuple[str, str, str]] = deque()
+    queue.append(("button", "low", "broadcaster"))
+
+    low = 0
+    high = 0
+    while queue:
+        sender, pulse, dest = queue.popleft()
+        if pulse == "high":
+            high += 1
+        else:
+            low += 1
+
+        if stop_pulse and pulse == stop_pulse and sender == stop_sender:
+            return low, high, True
+
+        if dest in modules:
+            to_send = modules[dest].process(pulse, sender)
+            if to_send:
+                for child, p in to_send:
+                    queue.append((dest, p, child))
+
+    return low, high, False
+
+
+def get_modules(lines: list[str]) -> dict[str, Module]:
+    modules: dict[str, Module] = {}
+    inputs = defaultdict(set)
+    for line in lines:
+        children = tuple([c.strip() for c in line[line.index(">") + 1 :].split(",")])
+        name = "broadcaster" if line.startswith("b") else line[1 : line.index("-") - 1]
+        for child in children:
+            inputs[child].add(name)
+        if line.startswith("&"):
+            modules[name] = Conjunction(children)
+        elif line.startswith("%"):
+            modules[name] = FlipFlop(children)
+        else:
+            modules[name] = Broadcast(children)
+
+    for name, module in modules.items():
+        if type(module) is Conjunction:
+            module.set_inputs(inputs[name])
+
+    return modules
+
+
+if __name__ == "__main__":
+    main()
---
+++ advent/advent2023/day21.py
@@ -0,0 +1,45 @@
+from collections.abc import Iterator
+
+import numpy as np
+import numpy.typing as npt
+
+from advent.utils.utils import Advent
+
+advent = Advent(21, 2023)
+
+deltas_4 = ((-1, 0), (0, -1), (0, 1), (1, 0))
+
+
+def main():
+    lines = advent.get_input_lines()
+    grid = np.array([[c for c in line] for line in lines], dtype=str)
+    start = np.where(grid == "S")
+    start = start[0][0], start[1][0]
+    nodes = {start}
+    for i in range(64):
+        nodes = step(grid, nodes)
+    advent.submit(1, len(nodes))
+
+
+def neighbors(
+    grid: npt.NDArray[np.str_], node: tuple[int, int]
+) -> Iterator[tuple[int, int]]:
+    r, c = node
+    maxr = len(grid) - 1
+    maxc = len(grid[0]) - 1
+    for dr, dc in deltas_4:
+        rr, rc = r + dr, c + dc
+        if 0 <= rr <= maxr and 0 <= rc <= maxc and grid[(rr, rc)] != "#":
+            yield (rr, rc)
+
+
+def step(grid: npt.NDArray[np.str_], nodes: set[tuple[int, int]]) -> set[tuple[int, int]]:
+    next_nodes = set()
+    for node in nodes:
+        for neighbor in neighbors(grid, node):
+            next_nodes.add(neighbor)
+    return next_nodes
+
+
+if __name__ == "__main__":
+    main()
