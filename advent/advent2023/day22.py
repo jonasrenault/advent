9842702e76cd03327c8e4da1114b591cfc17f161
Jonas Renault <jonasrenault@gmail.com>
2024-02-29 11:48:25 +0100

    2023 day22-23

---
+++ advent/advent2023/day22.py
@@ -0,0 +1,188 @@
+from __future__ import annotations
+
+from collections import defaultdict, deque
+from itertools import product
+from string import ascii_uppercase
+
+from advent.utils.utils import Advent
+
+advent = Advent(22, 2023)
+
+
+class Brick:
+    ids = product(ascii_uppercase, repeat=3)
+
+    def __init__(self, start: tuple[int, int, int], end: tuple[int, int, int]) -> None:
+        cubes = []
+        for i in range(len(start)):
+            if start[i] != end[i]:
+                for x in range(min(start[i], end[i]), max(start[i], end[i]) + 1):
+                    cubes.append(
+                        (
+                            x if i == 0 else start[0],
+                            x if i == 1 else start[1],
+                            x if i == 2 else start[2],
+                        )
+                    )
+        if not cubes:
+            cubes.append(start)
+        self.cubes = cubes
+        self.id = "".join(next(Brick.ids))
+
+    @property
+    def min_height(self) -> int:
+        return min([z for _, _, z in self.cubes])
+
+    @property
+    def max_height(self) -> int:
+        return max([z for _, _, z in self.cubes])
+
+    def set_height(self, height: int):
+        mh = self.min_height
+        self.cubes = [(x, y, height + z - mh) for (x, y, z) in self.cubes]
+
+    def overlaps(self, other: Brick) -> bool:
+        for l, r in product(self.cubes, other.cubes):  # noqa: E741
+            if l[0] == r[0] and l[1] == r[1]:
+                return True
+        return False
+
+    def supports(self, other: Brick) -> bool:
+        if self.max_height != other.min_height - 1:
+            return False
+        return self.overlaps(other)
+
+    def __repr__(self) -> str:
+        return self.id + ": " + " ".join([str(c) for c in self.cubes])
+
+
+def main():
+    lines = advent.get_input_lines()
+    bricks = get_bricks(lines)
+    bricks.sort(key=lambda x: x.min_height)
+    fall_bricks(bricks)
+    supported_by, supports = get_supports(bricks)
+    removable = get_removable(bricks, supported_by)
+    advent.submit(1, len(removable))
+
+    to_remove = set([b.id for b in bricks]) - removable
+    total = 0
+    for b in to_remove:
+        total += len(get_falling(b, supported_by, supports)) - 1
+    advent.submit(2, total)
+
+
+def get_falling(
+    removed_brick: str, supported_by: dict[str, set[str]], supports: dict[str, set[str]]
+) -> set[str]:
+    """
+    Get the bricks that would fall if you remove given brick.
+
+    Args:
+        removed_brick (str): the brick to remove
+        supported_by (dict[str, set[str]]): supported_by dict
+        supports (dict[str, set[str]]): supports dict
+
+    Returns:
+        set[str]: ids of bricks that would fall, including removed_brick
+    """
+    falling = set()
+    queue = deque([removed_brick])
+
+    while queue:
+        brick = queue.popleft()
+        falling.add(brick)
+        for supported in supports[brick]:
+            if all([b in falling for b in supported_by[supported]]):
+                queue.append(supported)
+
+    return falling
+
+
+def get_supports(
+    bricks: list[Brick],
+) -> tuple[dict[str, set[str]], dict[str, set[str]]]:
+    """
+    Get dicts of which bricks support / are supported by which other bricks.
+
+    Args:
+        bricks (list[Brick]): list of bricks
+
+    Returns:
+        tuple[dict[str, set[str]], dict[str, set[str]]]: dicts of supported_by
+        and supports
+    """
+    # for each brick, find which other bricks it rests on
+    supported_by = defaultdict(set)
+    for i in range(len(bricks)):
+        brick = bricks[i]
+        for ob in bricks[:i]:
+            if ob.supports(brick):
+                supported_by[brick.id].add(ob.id)
+
+    # invert the dict to find out for each brick which other bricks it supports
+    supports = defaultdict(set)
+    for brick_id, on_top in supported_by.items():
+        for ob_id in on_top:
+            supports[ob_id].add(brick_id)
+
+    return supported_by, supports
+
+
+def get_removable(bricks: list[Brick], supported_by: dict[str, set[str]]) -> set[str]:
+    """
+    Find bricks which can be safely removed.
+
+    Args:
+        bricks (list[Brick]): the list of bricks
+
+    Returns:
+        set[str]: set of bricks which can be removed
+    """
+    # any brick can be deleted unless it supports another brick by itself
+    removable = set([b.id for b in bricks])
+    for values in supported_by.values():
+        if len(values) == 1:
+            removable.discard(values.pop())
+    return removable
+
+
+def fall_bricks(bricks: list[Brick]):
+    """
+    Make bricks fall to their lowest possible height.
+
+    Args:
+        bricks (list[Brick]): list of bricks
+    """
+    # for each brick
+    for i in range(len(bricks)):
+        brick = bricks[i]
+        # if it's already on the floor, leave it
+        if brick.min_height == 1:
+            continue
+
+        # else, find bricks below which overlap with it
+        below = set()
+        for ob in bricks[:i]:
+            if brick.overlaps(ob):
+                below.add(ob)
+        if below:
+            # set height of brick to max height of bricks below + 1
+            brick.set_height(max([b.max_height for b in below]) + 1)
+        else:
+            # if no brick was found, it rests on the floor
+            brick.set_height(1)
+
+
+def get_bricks(lines: list[str]) -> list[Brick]:
+    bricks = []
+    for line in lines:
+        ll, lr = line.split("~")
+        left = tuple([int(x) for x in ll.split(",")])
+        right = tuple([int(x) for x in lr.split(",")])
+        bricks.append(Brick(left, right))  # type: ignore
+    return bricks
+
+
+if __name__ == "__main__":
+    main()
---
+++ advent/advent2023/day23.py
@@ -0,0 +1,202 @@
+import sys
+from collections import defaultdict, deque
+from collections.abc import Iterator
+
+import numpy as np
+import numpy.typing as npt
+
+from advent.utils.utils import Advent
+
+advent = Advent(23, 2023)
+
+deltas_4 = ((-1, 0), (0, -1), (0, 1), (1, 0))
+sys.setrecursionlimit(10**6)
+
+
+def main():
+    lines = advent.get_input_lines()
+    grid = np.array([[c for c in line] for line in lines], dtype=str)
+    h, w = grid.shape
+    start = (0, np.where(grid[0, :] == ".")[0][0])
+    end = (h - 1, np.where(grid[h - 1, :] == ".")[0][0])
+
+    graph = grid_to_graph(grid, start, end, False)
+    advent.submit(1, longest_path(graph, end, start, 0, set()))
+
+    graph = grid_to_graph(grid, start, end, True)
+    advent.submit(2, longest_path(graph, end, start, 0, set()))
+
+
+def grid_to_graph(
+    grid: npt.NDArray[np.str_],
+    src: tuple[int, int],
+    end: tuple[int, int],
+    ignore_slopes: bool = False,
+) -> dict[tuple[int, int], set[tuple[tuple[int, int], int]]]:
+    """
+    Transform grid to a graph. If ignore_slopes is True, slopes are ignored and graph is
+    simplified by merging nodes that have only two neighbors, keeping only intersections
+    as nodes.
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid
+        src (tuple[int, int]): the starting position
+        end (tuple[int, int]): the end position
+        ignore_slopes (bool, optional): ignore slopes. Defaults to False.
+
+    Returns:
+        dict[tuple[int, int], set[tuple[tuple[int, int], int]]]: the graph
+    """
+    graph = defaultdict(set)
+    seen = set()
+    queue = deque([src])
+
+    while queue:
+        node = queue.popleft()
+        if node in seen:
+            continue
+        seen.add(node)
+
+        if ignore_slopes:
+            for n, weight in adjacent_nodes(grid, node, src, end):
+                graph[node].add((n, weight))
+                queue.append(n)
+        else:
+            for n in neighbors(grid, node):
+                graph[node].add((n, 1))
+                queue.append(n)
+
+    return graph
+
+
+def adjacent_nodes(
+    grid: npt.NDArray[np.str_],
+    curr: tuple[int, int],
+    src: tuple[int, int],
+    end: tuple[int, int],
+) -> Iterator[tuple[tuple[int, int], int]]:
+    """
+    Yield the adjacent nodes and their distance from the curr position. Nodes are grid
+    positions that are either the start or end, or that have more than 2 neighbors.
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid
+        curr (tuple[int, int]): the current pos
+        src (tuple[int, int]): the start pos
+        end (tuple[int, int]): the end pos
+
+    Yields:
+        Iterator[tuple[tuple[int, int], int]]: iterator of nodes, distance
+    """
+    queue = [(curr, 0)]
+    visited = set()
+
+    while queue:
+        node, dist = queue.pop()
+        visited.add(node)
+
+        for n in neighbors(grid, node, True):
+            if n in visited:
+                continue
+
+            if is_node(grid, n, src, end, True):
+                yield n, dist + 1
+                continue
+
+            queue.append((n, dist + 1))
+
+
+def is_node(
+    grid: npt.NDArray[np.str_],
+    node: tuple[int, int],
+    src: tuple[int, int],
+    end: tuple[int, int],
+    ignore_slopes: bool,
+) -> bool:
+    """
+    Check if node position is a node in the graph, i.e. is the start or end pos, or has
+    more than two neighbors.
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid
+        node (tuple[int, int]): the curr pos
+        src (tuple[int, int]): the start pos
+        end (tuple[int, int]): the end pos
+        ignore_slopes (bool): ignore slopes
+
+    Returns:
+        bool: true if curr node is a node
+    """
+    return (
+        node == src or node == end or len(list(neighbors(grid, node, ignore_slopes))) > 2
+    )
+
+
+def neighbors(
+    grid: npt.NDArray[np.str_], node: tuple[int, int], ignore_slopes: bool = False
+) -> Iterator[tuple[int, int]]:
+    """
+    Iterator for the neighbors of given node.
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid
+        node (tuple[int, int]): the node
+        ignore_slopes (bool, optional): ignore_slopes. Defaults to False.
+
+    Yields:
+        Iterator[tuple[int, int]]: Iterator of neighbor positions
+    """
+    r, c = node
+    maxr = grid.shape[0] - 1
+    maxc = grid.shape[1] - 1
+    cell = grid[node]
+
+    if cell == "." or ignore_slopes:
+        for dr, dc in deltas_4:
+            rr, rc = r + dr, c + dc
+            if 0 <= rr <= maxr and 0 <= rc <= maxc and grid[rr, rc] != "#":
+                yield (rr, rc)
+    elif cell == ">":
+        yield (r, c + 1)
+    elif cell == "v":
+        yield (r + 1, c)
+
+
+def longest_path(
+    graph: dict[tuple[int, int], set[tuple[tuple[int, int], int]]],
+    end: tuple[int, int],
+    curr: tuple[int, int],
+    dist: int,
+    seen: set[tuple[int, int]],
+) -> int:
+    """
+    Find the longest path in the graph, from curr position to end, given dist and
+    traversed nodes to reach curr position.
+
+    Args:
+        graph (dict[tuple[int, int], set[tuple[tuple[int, int], int]]]): the graph
+        end (tuple[int, int]): the end position
+        curr (tuple[int, int]): the curr position
+        dist (int): distance to reach curr position
+        seen (set[tuple[int, int]]): nodes traversed to reach curr position
+
+    Returns:
+        int: the longest path
+    """
+    if curr == end:
+        return dist
+
+    best = 0
+    seen.add(curr)
+
+    for n, weight in graph[curr]:
+        if n in seen:
+            continue
+        best = max(best, longest_path(graph, end, n, dist + weight, seen))
+
+    seen.remove(curr)
+    return best
+
+
+if __name__ == "__main__":
+    main()
