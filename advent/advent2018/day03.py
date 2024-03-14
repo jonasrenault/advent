3a85f60fdd7cd2d99ee2bd6f39039b5e8a3798a0
Jonas Renault <jonasrenault@gmail.com>
2024-03-14 16:13:56 +0100

    day03

---
+++ advent/advent2018/day03.py
@@ -0,0 +1,25 @@
+import re
+
+import numpy as np
+from advent.utils.utils import Advent
+
+advent = Advent(3, 2018)
+
+
+def main():
+    lines = advent.get_input_lines()
+    fabric = np.zeros((2000, 2000), dtype=int)
+    for line in lines:
+        _, left, top, width, height = tuple(map(int, re.findall(r"-?\d+", line)))
+        fabric[top + 1 : top + height + 1, left + 1 : left + width + 1] += 1
+    advent.submit(1, np.count_nonzero(fabric > 1))
+
+    for line in lines:
+        id, left, top, width, height = tuple(map(int, re.findall(r"-?\d+", line)))
+        if np.all(fabric[top + 1 : top + height + 1, left + 1 : left + width + 1] == 1):
+            advent.submit(2, id)
+            break
+
+
+if __name__ == "__main__":
+    main()
