b94c26ad0b9c1f4d88a36723fb7f8d23b8bffc86
Jonas Renault <jonasrenault@gmail.com>
2024-04-02 17:16:25 +0200

    day05

---
+++ advent/advent2018/day05.py
@@ -0,0 +1,39 @@
+from advent.utils.utils import Advent
+
+advent = Advent(5, 2018)
+
+
+def main():
+    lines = advent.get_input_lines()
+    polymer = lines[0]
+    advent.submit(1, react(polymer))
+
+    advent.submit(
+        2,
+        min(
+            [
+                react(polymer.replace(x.lower(), "").replace(x.upper(), ""))
+                for x in set(polymer)
+            ]
+        ),
+    )
+
+
+def are_opposite(left: str, right: str) -> bool:
+    return left.lower() == right.lower() and (
+        (left.isupper() and right.islower()) or (left.islower() and right.isupper())
+    )
+
+
+def react(polymer: str) -> int:
+    buffer: list[str] = []
+    for c in polymer:
+        if buffer and are_opposite(c, buffer[-1]):
+            buffer.pop()
+        else:
+            buffer.append(c)
+    return len(buffer)
+
+
+if __name__ == "__main__":
+    main()
