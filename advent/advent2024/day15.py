a645596a1f5b12b99d532f31ef16d9206a38194c
Jonas Renault <jonasrenault@gmail.com>
2024-12-15 15:09:19 +0100

    day15 part2

--- advent/advent2024/day15.py
+++ advent/advent2024/day15.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 import numpy as np
 import numpy.typing as npt
 
@@ -15,6 +17,35 @@ def main():
         [line for line in lines if line.startswith(("^", "<", ">", "v"))]
     )
 
+    run(grid, directions)
+    advent.submit(1, score(grid))
+
+    grid2 = np.array(
+        [
+            [
+                c
+                for c in line.replace("#", "##")
+                .replace("O", "[]")
+                .replace(".", "..")
+                .replace("@", "@.")
+            ]
+            for line in lines
+            if line.startswith("#")
+        ]
+    )
+
+    run(grid2, directions)
+    advent.submit(2, score(grid2, "["))
+
+
+def run(grid: npt.NDArray[np.str_], directions: str):
+    """
+    Run all movements from directions on grid
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid.
+        directions (str): string of directions
+    """
     robot = tuple(np.argwhere(grid == "@")[0])
 
     for dir in directions:
@@ -22,28 +53,118 @@ def main():
         if moved:
             robot = tuple(np.add(robot, DIR[dir]))
 
-    advent.submit(1, score(grid))
+
+def print_grid(grid: npt.NDArray[np.str_]):
+    """
+    Print a grid.
+
+    Args:
+        grid (npt.NDArray[np.str_]): grid to print.
+    """
+    for row in grid:
+        print("".join(row))
 
 
-def score(grid: npt.NDArray[np.str_]) -> int:
-    return sum([100 * x + y for x, y in zip(*np.where(grid == "O"))])
+def score(grid: npt.NDArray[np.str_], target: str = "O") -> int:
+    """
+    Return grid's score
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid.
+        target (str, optional): target to score, either O or [. Defaults to "O".
+
+    Returns:
+        int: the score
+    """
+    return sum([100 * x + y for x, y in zip(*np.where(grid == target))])
+
+
+def to_move(
+    grid: npt.NDArray[np.str_], node: tuple[int, int], dir: str
+) -> set[tuple[int, int]]:
+    """
+    Return set of nodes to move together
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid.
+        node (tuple[int, int]): current node to move
+        dir (str): direction of movement
+
+    Returns:
+        set[tuple[int, int]]: set of nodes to move together.
+    """
+    x, y = node
+    dx, dy = DIR[dir]
+
+    nodes = set((node,))
+    if grid[node] in "[]" and dir in ("^v"):
+        other_node = (x, y - 1) if grid[node] == "]" else (x, y + 1)
+        nodes.add(other_node)
+
+    res = set(nodes)
+    for x, y in nodes:
+        neighbor = (x + dx, y + dy)
+        if not (grid[neighbor] == "." or grid[neighbor] == "#"):
+            res |= to_move(grid, neighbor, dir)
+
+    return res
+
+
+def can_move(grid: npt.NDArray[np.str_], nodes: set[tuple[int, int]], dir: str) -> bool:
+    """
+    Check if a set a nodes can move in given direction.
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid.
+        nodes (set[tuple[int, int]]): set of nodes to move.
+        dir (str): the direction
+
+    Returns:
+        bool: whether the nodes can move or not.
+    """
+    dx, dy = DIR[dir]
+    for x, y in nodes:
+        neighbor = (x + dx, y + dy)
+        if not (neighbor in nodes or grid[neighbor] == "."):
+            return False
+    return True
+
+
+def move_nodes(grid: npt.NDArray[np.str_], nodes: set[tuple[int, int]], dir: str):
+    """
+    Move a set of nodes in given direction
+
+    Args:
+        grid (npt.NDArray[np.str_]): the grid
+        nodes (set[tuple[int, int]]): set of nodes to move
+        dir (str): the direction
+    """
+    dx, dy = DIR[dir]
+    sorted_nodes = list(nodes)
+    sorted_nodes.sort(
+        key=itemgetter(1) if dir in "<>" else itemgetter(0), reverse=dir in ">v"
+    )
+    for x, y in sorted_nodes:
+        grid[(x + dx, y + dy)] = grid[(x, y)]
+        grid[(x, y)] = "."
 
 
 def move(grid: npt.NDArray[np.str_], node: tuple[int, int], dir: str) -> bool:
-    dx, dy = DIR[dir]
-    x, y = node
-    moved = False
-    if grid[(x + dx, y + dy)] == ".":
-        moved = True
-    elif grid[(x + dx, y + dy)] == "#":
-        moved = False
-    else:
-        moved = move(grid, (x + dx, y + dy), dir)
+    """
+    Move the given node and all boxes in given direction.
 
+    Args:
+        grid (npt.NDArray[np.str_]): the grid.
+        node (tuple[int, int]): the node to move.
+        dir (str): the direction
+
+    Returns:
+        bool: Whether tne node moved or not.
+    """
+    nodes = to_move(grid, node, dir)
+    moved = can_move(grid, nodes, dir)
     if moved:
-        grid[(x + dx, y + dy)] = grid[node]
-        grid[node] = "."
-
+        move_nodes(grid, nodes, dir)
     return moved
 
 
